96da62c97e955e3924b195778c51083000ef00a8
Subrat <subratsinghsks_cse16@its.edu.in>
2022-12-31 20:10:15 -0500

    template done

---
+++ tool/bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const arg = require('arg');
+const chalk = require('chalk');
+
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
+
+  if (args['--start']) {
+    console.log(chalk.bgCyanBright('starting the app'));
+  }
+} catch (e) {
+  console.log(chalk.yellow(e.message));
+  console.log();
+  usage();
+}
+
+function usage() {
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
+  
+}
